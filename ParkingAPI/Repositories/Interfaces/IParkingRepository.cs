b3587bc7f4da4937b2911701c1f2a259f74866d7
rodriguesnivea <rodriguesnivea779@gmail.com>
2023-12-06 22:13:40 -0300

    add interfaces

---
+++ ParkingAPI/Repositories/Interfaces/IParkingRepository.cs
@@ -0,0 +1,14 @@
+﻿using ParkingAPI.Entities;
+using ParkingAPI.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace ParkingAPI.Repositories.Interfaces
+{
+    public interface IParkingRepository : IBaseRepository<ParkingEntiy>
+    {
+        Task<bool> CheckinAsync(ParkingEntiy entity);
+        Task<bool> CheckoutAsync(ParkingEntiy entity);
+        Task <ParkingEntiy> FindParkedVehicleAsync(Guid companyId, Guid vehicleId);
+    }
+}
--- ParkingAPI/Repositories/Interfaces/IVehicleRepository.cs
+++ ParkingAPI/Repositories/Interfaces/IVehicleRepository.cs
@@ -4,5 +4,6 @@ namespace ParkingAPI.Repositories.Interfaces
 {
     public interface IVehicleRepository : IBaseRepository<VehicleEntity> 
     {
+
     }
 }
