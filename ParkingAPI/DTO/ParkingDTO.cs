be4e0bfd620296f0461f99534e772cb42ac07575
rodriguesnivea <rodriguesnivea779@gmail.com>
2023-12-06 22:13:25 -0300

    add map e to

---
+++ ParkingAPI/DTO/ParkingDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ParkingAPI.DTO
+{
+    public class ParkingDTO
+    {
+        public CompanyDTO Company { get; set; }
+        public VehicleDTO Vehicle { get; set; }
+    }
+}
---
+++ ParkingAPI/Mappers/ParkingMap.cs
@@ -0,0 +1,60 @@
+﻿using ParkingAPI.DTO;
+using ParkingAPI.Entities;
+using ParkingAPI.Models;
+
+namespace ParkingAPI.Mappers
+{
+    public class ParkingMap
+    {
+        public static ParkingEntiy ModelToEntity(ParkingModel model)
+        {
+            ParkingEntiy entity = new ParkingEntiy()
+            {
+                Id = model.Id,
+                CreateAT = model.CreateAT,
+                UpdateAt = model.UpdateAt,
+                CompanyId = model.CompanyId,
+                VehicleId = model.VehicleId,
+                IsParked = model.IsParked
+            };
+
+            return entity;
+        }
+
+        public static ParkingModel EntityToModel(ParkingEntiy entity)
+        {
+            ParkingModel model = new ParkingModel()
+            {
+                Id = entity.Id,
+                CreateAT = entity.CreateAT,
+                UpdateAt = entity.UpdateAt,
+                CompanyId = entity.CompanyId,
+                VehicleId = entity.VehicleId,
+                IsParked = entity.IsParked,
+                Company = CompanyMap.EntityToModel(entity.Company),
+                Vehicle = VehicleMap.EntityToModel(entity.Vehicle)
+            };
+            
+            return model;
+        }
+
+
+        public static ParkingModel DtoToModel(ParkingDTO dto)
+        {
+            ParkingModel model = new ParkingModel()
+            {
+                
+            };
+            return model;
+        }
+
+        public static ParkingDTO ModelToDto(ParkingModel model)
+        {
+            ParkingDTO dto = new ParkingDTO()
+            {
+                
+            };
+            return dto;
+        }
+    }
+}
