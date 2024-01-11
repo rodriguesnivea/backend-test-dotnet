ee3ca5ca21810d91966695547a9f1fd939b1df76
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-01-10 23:11:50 -0300

    add migração

---
+++ ParkingAPI/Migrations/20240111015216_unique_column.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingAPI.Migrations
+{
+    public partial class unique_column : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicle_plate",
+                table: "Vehicle",
+                column: "plate",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Vehicle_plate",
+                table: "Vehicle");
+        }
+    }
+}
