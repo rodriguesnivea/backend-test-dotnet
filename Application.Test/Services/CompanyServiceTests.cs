7f7ebf136ab94f41e7f04fdab8a586efee3bc260
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-02-05 23:46:03 -0300

    Adicionando testes unitários para company e Vehicle

---
+++ Application.Test/Services/CompanyServiceTests.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using ParkingAPI.Entities;
+using ParkingAPI.Exceptions;
+using ParkingAPI.Models;
+using ParkingAPI.Repositories.Interfaces;
+using ParkingAPI.Services;
+using ParkingAPI.Services.Interfaces;
+using ParkingAPI.Tracing.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.Test.Services
+{
+    public class CompanyServiceTests
+    {
+        private CompanyService _companyService;
+        private Mock<ICompanyRepository> _companyRepositoryMock;
+        private Mock<ILogger<CompanyService>> _loggerMock;
+        private Mock<ITracingService> _tracingServiceMock;
+
+        public CompanyServiceTests()
+        {
+            _companyRepositoryMock = new Mock<ICompanyRepository>();
+            _loggerMock = new Mock<ILogger<CompanyService>>();
+            _tracingServiceMock = new Mock<ITracingService>();
+            _companyService = new CompanyService(_companyRepositoryMock.Object, _loggerMock.Object, _tracingServiceMock.Object);
+
+        }
+
+        [Fact]
+        public async void CreateAsync_CriaEmpresaComSucesso()
+        {
+            var address = new AddressModel
+            { Street = "Pedro S. Delton", City = "Rio Branco", State = "Ceara", Country = "Brasil", Number = 123 };
+            var companyTobeSaved = new CompanyModel
+            { Name = "CAutomotiva", CNPJ = "12539922385646", NumberMotorcycies = 2, NumberCars = 3, Phone = "(95) 3970-4294", Address = address };
+
+            var result = await _companyService.CreateAsync(companyTobeSaved);
+            Assert.NotNull(result);
+            Assert.Equal(companyTobeSaved.Id, result.Id);
+            Assert.Equal(companyTobeSaved.Name, result.Name);
+            Assert.Equal(companyTobeSaved.CNPJ, result.CNPJ);
+            Assert.Equal(companyTobeSaved.NumberMotorcycies, result.NumberMotorcycies);
+            Assert.Equal(companyTobeSaved.NumberCars, result.NumberCars);
+            Assert.Equal(companyTobeSaved.Phone, result.Phone);
+            Assert.Equal(companyTobeSaved.Address.Street, result.Address.Street);
+            Assert.Equal(companyTobeSaved.Address.City, result.Address.City);
+            Assert.Equal(companyTobeSaved.Address.State, result.Address.State);
+            Assert.Equal(companyTobeSaved.Address.Country, result.Address.Country);
+            Assert.Equal(companyTobeSaved.Address.Number, result.Address.Number);
+        }
+
+        [Fact]
+        public async void DeleteAsync_DeletaEmpresaComSucesso()
+        {
+            Guid idToBeDeleed = Guid.NewGuid();
+            _companyRepositoryMock.Setup(repository => repository.Exist(idToBeDeleed)).ReturnsAsync(true);
+            await _companyService.DeleteAsync(idToBeDeleed);
+            _companyRepositoryMock.Verify(repository => repository.DeleteCompanyAsync(idToBeDeleed), Times.Once());
+        }
+
+        [Fact]
+        public async void DeleteAsync_LancaExcecaoVeiculoNaoEncontrado()
+        {
+            Guid idToBeDeleed = Guid.NewGuid();
+            _companyRepositoryMock.Setup(repository => repository.Exist(idToBeDeleed)).ReturnsAsync(false);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _companyService.DeleteAsync(idToBeDeleed));
+            Assert.Equal(exception.ApplicationError, ApplicationError.COMPANY_NOT_FOUND_EXCEPTION);
+            _companyRepositoryMock.Verify(repository => repository.DeleteCompanyAsync(idToBeDeleed), Times.Never());
+        }
+
+        [Fact]
+        public async void Exist_ValidaExistenciaDeUmaEmpresa()
+        {
+            Guid idToBeChecked = Guid.NewGuid();
+            await _companyService.Exist(idToBeChecked);
+            _companyRepositoryMock.Verify(repository => repository.Exist(idToBeChecked), Times.Once);
+        }
+
+        [Fact]
+        public async void GetAllAsync_BuscaTodasEmpresasComSucesso()
+        {
+            var address = new AddressEntity
+            { Street = "Pedro S. Delton", City = "Rio Branco", State = "Ceara", Country = "Brasil", Number = 123 };
+            var company = new CompanyEntity
+            { Name = "CAutomotiva", CNPJ = "12539922385646", NumberMotorcycies = 2, NumberCars = 3, Phone = "(95) 3970-4294", Address = address };
+
+            var expected = new List<CompanyEntity> { company };
+            _companyRepositoryMock.Setup(repository => repository.FindAllAsync()).ReturnsAsync(expected);
+            var result = await _companyService.GetAllAsync();
+
+            Assert.NotNull(result);
+            Assert.NotNull(expected[0].Address);
+            Assert.Equal(expected[0].Name, result[0].Name);
+            Assert.Equal(expected[0].CNPJ, result[0].CNPJ);
+            Assert.Equal(expected[0].NumberCars, result[0].NumberCars);
+            Assert.Equal(expected[0].NumberMotorcycies, result[0].NumberMotorcycies);
+            Assert.Equal(expected[0].Phone, result[0].Phone);
+
+        }
+
+        [Fact]
+        public async void GetAllAsync_LancaExcecaoNaoContem()
+        {
+            _companyRepositoryMock.Setup(repositorry => repositorry.FindAllAsync()).ReturnsAsync(new List<CompanyEntity> { });
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _companyService.GetAllAsync());
+            Assert.Equal(exception.ApplicationError, ApplicationError.COMPANY_NO_CONTENT_EXCEPTION);
+        }
+
+        [Fact]
+        public async void GetAsync_BuscaEmpresaComSucesso()
+        {
+            Guid id = Guid.NewGuid();
+            var address = new AddressEntity
+            { Street = "Pedro S. Delton", City = "Rio Branco", State = "Ceara", Country = "Brasil", Number = 123 };
+            var company = new CompanyEntity
+            { Id = id, Name = "CAutomotiva", CNPJ = "12539922385646", NumberMotorcycies = 2, NumberCars = 3, Phone = "(95) 3970-4294", Address = address };
+
+            _companyRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(true);
+            _companyRepositoryMock.Setup(repository => repository.GetAsync(id)).ReturnsAsync(company);
+
+            var result = await _companyService.GetAsync(id);
+            Assert.NotNull(result);
+            Assert.NotNull(result.Address);
+            Assert.True(result.Id != default);
+            Assert.Equal(company.Id, result.Id);
+            Assert.Equal(company.Name, result.Name);
+            Assert.Equal(company.CNPJ, result.CNPJ);
+            Assert.Equal(company.NumberMotorcycies, result.NumberMotorcycies);
+            Assert.Equal(company.NumberCars, result.NumberCars);
+            Assert.Equal(company.Phone, result.Phone);
+
+        }
+
+        [Fact]
+        public async void GetAsync_LancaExcecaoNaoEncontrado()
+        {
+            Guid id = Guid.NewGuid();
+            _companyRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(false);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _companyService.GetAsync(id));
+            Assert.Equal(exception.ApplicationError, ApplicationError.COMPANY_NOT_FOUND_EXCEPTION);
+        }
+
+        [Fact]
+        public async void UpdateAsync_AtualizaEmpresaComSucesso()
+        {
+            var id = Guid.NewGuid();
+            var addressTobeSaved = new AddressModel
+            { Street = "Pedro S. Delton", City = "Rio Branco", State = "Ceara", Country = "Brasil", Number = 123 };
+            var companyTobeSaved = new CompanyModel
+            { Name = "CAutomotiva", CNPJ = "12539922385646", NumberMotorcycies = 2, NumberCars = 3, Phone = "(95) 3970-4294", Address = addressTobeSaved };
+            _companyRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(true);
+            await _companyService.UpdateAsync(id, companyTobeSaved);
+            _companyRepositoryMock.Verify(repository => repository.Exist(id), Times.Once);
+        }
+
+        [Fact]
+        public async void UpdateAsync_LancaExcecaoEmpresaNaoEncontrada()
+        {
+            var id = Guid.NewGuid();
+            var addressTobeSaved = new AddressModel
+            { Street = "Pedro S. Delton", City = "Rio Branco", State = "Ceara", Country = "Brasil", Number = 123 };
+            var companyTobeSaved = new CompanyModel
+            { Name = "CAutomotiva", CNPJ = "12539922385646", NumberMotorcycies = 2, NumberCars = 3, Phone = "(95) 3970-4294", Address = addressTobeSaved };
+            _companyRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(false);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _companyService.UpdateAsync(id, companyTobeSaved));
+            Assert.Equal(exception.ApplicationError, ApplicationError.COMPANY_NOT_FOUND_EXCEPTION);
+        }
+    }
+}
---
+++ Application.Test/Services/VehicleServiceTests.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using ParkingAPI.Entities;
+using ParkingAPI.Enums;
+using ParkingAPI.Exceptions;
+using ParkingAPI.Models;
+using ParkingAPI.Repositories.Interfaces;
+using ParkingAPI.Services;
+using ParkingAPI.Tracing.Interfaces;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Application.Tests.Service
+{
+    public class VehicleServiceTests
+    {
+        private VehicleService _vehicleService;
+        private Mock<ILogger<VehicleService>> _loggerMock;
+        private Mock<IVehicleRepository> _vehicleRepositoryMock;
+        private Mock<ITracingService> _tracingServiceMock;
+
+        public VehicleServiceTests()
+        {
+            _vehicleRepositoryMock = new Mock<IVehicleRepository>();
+            _loggerMock = new Mock<ILogger<VehicleService>>();
+            _tracingServiceMock = new Mock<ITracingService>();
+            _vehicleService = new VehicleService(_vehicleRepositoryMock.Object, _loggerMock.Object, _tracingServiceMock.Object);
+        }
+
+        [Fact]
+        public async void CreateAsync_CriaVeiculoComSucesso()
+        {
+            var vehicleToBeSaved = new VehicleModel { Brand = "Fiat", Model = "Grande", Color = "Azul", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Car };
+            _vehicleRepositoryMock.Setup(repository => repository.PlateExist(vehicleToBeSaved.Plate)).ReturnsAsync(false);
+            var result = await _vehicleService.CreateAsync(vehicleToBeSaved);
+            Assert.NotNull(result);
+            Assert.Equal(vehicleToBeSaved.Id, result.Id);
+            Assert.Equal(vehicleToBeSaved.Brand, result.Brand);
+            Assert.Equal(vehicleToBeSaved.Model, result.Model);
+            Assert.Equal(vehicleToBeSaved.Color, result.Color);
+            Assert.Equal(vehicleToBeSaved.Plate, result.Plate);
+            Assert.Equal(vehicleToBeSaved.typeVehicle, result.typeVehicle);
+            Assert.True(result.Id != default);
+        }
+
+        [Fact]
+        public async void CreateAsync_LancaExcecaoPlacaConflito()
+        {
+            var vehicleToBeSaved = new VehicleModel { Brand = "Fiat", Model = "Grande", Color = "Azul", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Car };
+            _vehicleRepositoryMock.Setup(repository => repository.PlateExist(vehicleToBeSaved.Plate)).ReturnsAsync(true);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _vehicleService.CreateAsync(vehicleToBeSaved));
+            Assert.Equal(ApplicationError.VEHICLE_PLATE_CONFLICT_EXCEPTION, exception.ApplicationError);
+        }
+
+        [Fact]
+        public async void DeleteAsync_DeletaVeiculoComSucesso()
+        {
+            Guid idToBeDeleted = Guid.NewGuid();
+            _vehicleRepositoryMock.Setup(repository => repository.Exist(idToBeDeleted)).ReturnsAsync(true);
+            await _vehicleService.DeleteAsync(idToBeDeleted);
+            _vehicleRepositoryMock.Verify(repository => repository.DeleteAsync(idToBeDeleted), Times.Once);
+        }
+
+        [Fact]
+        public async void DeleteAsync_LancaExcecaoVeiculoNaoEncontrado()
+        {
+            Guid idToBeDeleted = Guid.NewGuid();
+            _vehicleRepositoryMock.Setup(repository => repository.Exist(idToBeDeleted)).ReturnsAsync(false);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _vehicleService.DeleteAsync(idToBeDeleted));
+            Assert.Equal(ApplicationError.VEHICLE_NOT_FOUND_EXCEPTION, exception.ApplicationError);
+            _vehicleRepositoryMock.Verify(repository => repository.DeleteAsync(idToBeDeleted), Times.Never);
+        }
+
+        [Fact]
+        public async void Exist_ValidaExistenciaDeUmVeiculo()
+        {
+            Guid idToBeChecked = Guid.NewGuid();
+            await _vehicleService.Exist(idToBeChecked);
+            _vehicleRepositoryMock.Verify(repository => repository.Exist(idToBeChecked), Times.Once);
+        }
+
+        [Fact]
+        public async void GetAllAsync_BuscaVeiculosComSucesso()
+        {
+            var expected = new List<VehicleEntity> { new VehicleEntity
+            {Id = Guid.NewGuid(), Brand = "Fiat", Model = "Grande", Color = "Azul", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Car } };
+            _vehicleRepositoryMock.Setup(repository => repository.GetAllAsync()).ReturnsAsync(expected);
+            var result = await _vehicleService.GetAllAsync();
+            Assert.NotNull(result);
+            Assert.Equal(expected[0].Id, result[0].Id);
+            Assert.Equal(expected[0].Model, result[0].Model);
+            Assert.Equal(expected[0].Brand, result[0].Brand);
+            Assert.Equal(expected[0].Color, result[0].Color);
+            Assert.Equal(expected[0].Plate, result[0].Plate);
+            Assert.Equal(expected[0].typeVehicle, result[0].typeVehicle);
+        }
+
+        [Fact]
+        public async void GetAllAsync_BuscaVeiculosLancaExcecaoNenhumVeiculoEncontrado()
+        {
+            var expected = new List<VehicleEntity>();
+            _vehicleRepositoryMock.Setup(repository => repository.GetAllAsync()).ReturnsAsync(expected);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _vehicleService.GetAllAsync());
+            Assert.Equal(ApplicationError.VEHICLE_NO_CONTENT_EXCEPTION, exception.ApplicationError);
+
+        }
+        [Fact]
+        public async void GetAsync_BuscaVeiculoPorIdComSucesso()
+        {
+            var id = Guid.NewGuid();
+            var expected = new VehicleEntity { Id = Guid.NewGuid(), Brand = "Fiat", Model = "Grande", Color = "Azul", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Car };
+            _vehicleRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(true);
+            _vehicleRepositoryMock.Setup(repository => repository.GetAsync(id)).ReturnsAsync(expected);
+
+            var result = await _vehicleService.GetAsync(id);
+            Assert.NotNull(result);
+            Assert.Equal(expected.Brand, result.Brand);
+            Assert.Equal(expected.Model, result.Model);
+            Assert.Equal(expected.Color, result.Color);
+            Assert.Equal(expected.Plate, result.Plate);
+            Assert.Equal(expected.typeVehicle, result.typeVehicle);
+            Assert.True(result.Id != default);
+        }
+
+        [Fact]
+        public async void GetAsync_BuscaVeiculoPorIdLancaExcecao()
+        {
+            var id = Guid.NewGuid();
+            _vehicleRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(false);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _vehicleService.GetAsync(id));
+            Assert.Equal(ApplicationError.VEHICLE_NOT_FOUND_EXCEPTION, exception.ApplicationError);
+            _vehicleRepositoryMock.Verify(repository => repository.GetAsync(id), Times.Never);
+        }
+
+        [Fact]
+        public async void UpdateAsync_AtualizaTodosOsCamposExcetoPlacaDoVeiculoComSucesso()
+        {
+            var id = Guid.NewGuid();
+            var vehicleToBeSaved = new VehicleModel { Brand = "Corola", Model = "Medio", Color = "Cinza", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Motocycle };
+            var currentVehicle = new VehicleEntity { Brand = "Fiat", Model = "Grande", Color = "Azul", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Car };
+            _vehicleRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(true);
+            _vehicleRepositoryMock.Setup(repository => repository.GetAsync(id)).ReturnsAsync(currentVehicle);
+
+            var result = await _vehicleService.UpdateAsync(id, vehicleToBeSaved);
+            Assert.True(result.Id != default);
+            Assert.NotNull(result);
+            Assert.Equal(currentVehicle.Plate, result.Plate);
+            Assert.NotEqual(currentVehicle.Brand, result.Brand);
+            Assert.NotEqual(currentVehicle.Model, result.Model);
+            Assert.NotEqual(currentVehicle.Color, result.Color);
+            Assert.NotEqual(currentVehicle.typeVehicle, result.typeVehicle);
+        }
+
+        [Fact]
+        public async void UpdateAsync_LancaExcecaoVeiculoNaoEncontrado()
+        {
+            var id = Guid.NewGuid();
+            var vehicleToBeSaved = new VehicleModel { Brand = "Corola", Model = "Medio", Color = "Cinza", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Motocycle };
+            _vehicleRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(false);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _vehicleService.UpdateAsync(id, vehicleToBeSaved));
+            Assert.Equal(ApplicationError.VEHICLE_NOT_FOUND_EXCEPTION, exception.ApplicationError);
+        }
+
+        [Fact]
+        public async void UpdateAsync_LancaExcecaoPlacaComConflitos()
+        {
+            var id = Guid.NewGuid();
+            var vehicleToBeSaved = new VehicleModel { Id = id, Brand = "Corola", Model = "Medio", Color = "Cinza", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Motocycle };
+            var currentVehicle = new VehicleEntity {Id = id, Brand = "Fiat", Model = "Grande", Color = "Azul", Plate = "ads-345", typeVehicle = TypeVehicleEnum.Car };
+            var vehicleFromPlate = new VehicleEntity {Id = Guid.NewGuid(), Brand = "Fiat", Model = "Grande", Color = "Azul", Plate = "ads-111", typeVehicle = TypeVehicleEnum.Car };
+            _vehicleRepositoryMock.Setup(repository => repository.Exist(id)).ReturnsAsync(true);
+            _vehicleRepositoryMock.Setup(repository => repository.GetAsync(id)).ReturnsAsync(currentVehicle);
+            _vehicleRepositoryMock.Setup(repository => repository.GetByPlateAsync(vehicleToBeSaved.Plate)).ReturnsAsync(vehicleFromPlate);
+            var exception = await Assert.ThrowsAsync<ServiceException>(() => _vehicleService.UpdateAsync(id, vehicleToBeSaved));
+            Assert.Equal(ApplicationError.VEHICLE_PLATE_CONFLICT_EXCEPTION, exception.ApplicationError);
+
+        }
+    }
+}
