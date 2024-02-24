0991abe4a26425b0a5cc90e87b7d7b6179125fef
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-02-23 22:49:20 -0300

    Adicionando novos DTOS para histórico de estacionamento

---
+++ ParkingAPI/DTO/DetaildReportDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkingAPI.DTO
+{
+    public class DetaildReportDTO
+    {
+        public Guid CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Cnpj { get; set; }
+        public int TotalCheckinCar { get; set; }
+        public int TotalCheckoutCar { get; set; }
+        public int TotalCheckinMotorcycle { get; set; }
+        public int TotalCheckoutMotorcycle { get; set; }
+        public List<ParkingHistoryDTO> parkingHistory{ get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime{ get; set; }
+    }
+}
---
+++ ParkingAPI/DTO/ParkingHistoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParkingAPI.DTO
+{
+    public class ParkingHistoryDTO
+    {
+        public VehicleDTO vehicle { get; set;}
+        public DateTime DateCheckin{ get; set;}
+        public DateTime DateCheckout { get; set;}
+        
+    }
+}
