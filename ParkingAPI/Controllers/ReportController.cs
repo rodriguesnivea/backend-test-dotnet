92b4b94a3b4109cefd9c7499c0e68d6e0c080394
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-03-05 23:37:34 -0300

    Implementa controller para Relatórios de estacionamento

---
+++ ParkingAPI/Controllers/ReportController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingAPI.Services.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace ParkingAPI.Controllers
+{
+    [Route("api/v1/reports")]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpPost("{companyId}")]
+        public async Task<ActionResult> GetReport(Guid companyId)
+        {
+            var report = await _reportService.GetdReportAsync(companyId);
+            return Ok(report);
+        }
+
+        [HttpPost("{companyId}/detailed")]
+        public async Task<ActionResult> GetDetaildReport (Guid companyId, [DataType(DataType.Date)] DateTime startTime, [DataType(DataType.Date)] DateTime endTime)
+        {
+            var report = await _reportService.GetDetaildReportAsync(companyId, startTime, endTime);
+            return Ok(report);
+        }
+    }
+}
