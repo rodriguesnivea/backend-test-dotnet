e9825eb81eee28d76e7d66543ab46162fe7c5fc2
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-03-05 23:41:32 -0300

    Implementa novos métodos no Repository de estacionamento para buscar dados para preencher o Relatório de Estacionamento

--- ParkingAPI/Repositories/Interfaces/IParkingRepository.cs
+++ ParkingAPI/Repositories/Interfaces/IParkingRepository.cs
@@ -1,6 +1,8 @@
 ﻿using ParkingAPI.Entities;
+using ParkingAPI.Enums;
 using ParkingAPI.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ParkingAPI.Repositories.Interfaces
@@ -11,5 +13,12 @@ namespace ParkingAPI.Repositories.Interfaces
         Task<bool> CheckoutAsync(ParkingEntiy entity);
         Task<int> GetNumberOfParkedVehicles(CompanyEntity company, VehicleEntity vehicle);
         Task <ParkingEntiy> FindParkedVehicleAsync(Guid companyId, Guid vehicleId);
+        Task<int> GetTotalVehicleCheckinsByCompany(Guid companyId);
+        Task<int> GetTotalVehicleCheckoutsByCompany(Guid companyId);
+        Task<int> GetTotalCarCheckinsByCompanyInTimeRange(Guid companyId, DateTime StartTime, DateTime EndTime);
+        Task<int> GetTotalMotorcycleCheckinsByCompanyInTimeRange(Guid companyId, DateTime StartTime, DateTime EndTime);
+        Task<int> GetTotalCarCheckoutsByCompanyInTimeRange(Guid companyId, DateTime StartTime, DateTime EndTime);
+        Task<int> GetTotalMotorcycleCheckoutsByCompanyInTimeRange(Guid companyId, DateTime StartTime, DateTime EndTime);
+        Task<List<ParkingEntiy>> GetParkingHistoryByCompanyInTimeRange(Guid companyId, DateTime StartTime, DateTime EndTime);
     }
 }
--- ParkingAPI/Repositories/ParkingRepository.cs
+++ ParkingAPI/Repositories/ParkingRepository.cs
@@ -2,10 +2,12 @@
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using ParkingAPI.Context;
 using ParkingAPI.Entities;
+using ParkingAPI.Enums;
 using ParkingAPI.Exceptions;
 using ParkingAPI.Models;
 using ParkingAPI.Repositories.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -40,7 +42,6 @@ namespace ParkingAPI.Repositories
                 .Include(p => p.Company)
                 .Include(p => p.Vehicle)
                 .FirstOrDefaultAsync(p => p.CompanyId == companyId && p.VehicleId == vehicleId && p.IsParked);
-
             return entity;
         }
 
@@ -51,6 +52,69 @@ namespace ParkingAPI.Repositories
                 .Where(p => p.Vehicle.typeVehicle == vehicle.typeVehicle && p.CompanyId == company.Id && p.IsParked)
                 .CountAsync();
             return count;
+        }
+
+        public async Task<int> GetTotalVehicleCheckinsByCompany(Guid companyId)
+        {
+            var count = await _DbSet
+               .Include(p => p.Vehicle)
+               .Where(p => p.CompanyId == companyId)
+               .CountAsync();
+            return count;
+        }
+
+        public async Task<int> GetTotalVehicleCheckoutsByCompany(Guid companyId)
+        {
+            var count = await _DbSet
+               .Include(p => p.Vehicle)
+               .Where(p => !p.IsParked && p.CompanyId == companyId)
+               .CountAsync();
+            return count;
+        }
+
+        public async Task<int> GetTotalCarCheckinsByCompanyInTimeRange(Guid companyId, DateTime startTime, DateTime endTime)
+        {
+            var count = await _DbSet
+               .Include(p => p.Vehicle)
+               .Where(p => p.Vehicle.typeVehicle == TypeVehicleEnum.Car && p.CompanyId == companyId && p.CreateAT >= startTime && p.UpdateAt <= endTime)
+               .CountAsync();
+            return count;
+        }
+
+        public async Task<int> GetTotalMotorcycleCheckinsByCompanyInTimeRange(Guid companyId, DateTime startTime, DateTime endTime)
+        {
+            var count = await _DbSet
+                .Include(p => p.Vehicle)
+                .Where(p => p.Vehicle.typeVehicle == TypeVehicleEnum.Motocycle && p.CompanyId == companyId)
+                .CountAsync();
+            return count;
+        }
+
+        public async Task<int> GetTotalCarCheckoutsByCompanyInTimeRange(Guid companyId, DateTime startTime, DateTime endTime)
+        {
+            var count = await _DbSet
+               .Include(p => p.Vehicle)
+               .Where(p => !p.IsParked && p.Vehicle.typeVehicle == TypeVehicleEnum.Car && p.CompanyId == companyId && p.CreateAT >= startTime && p.UpdateAt <= endTime)
+               .CountAsync();
+            return count;
+        }
+
+        public async Task<int> GetTotalMotorcycleCheckoutsByCompanyInTimeRange(Guid companyId, DateTime startTime, DateTime endTime)
+        {
+            var count = await _DbSet
+               .Include(p => p.Vehicle)
+               .Where(p => !p.IsParked && p.Vehicle.typeVehicle == TypeVehicleEnum.Car && p.CompanyId == companyId && p.CreateAT >= startTime && p.UpdateAt <= endTime)
+               .CountAsync();
+            return count;
+        }
+
+        public async Task<List<ParkingEntiy>> GetParkingHistoryByCompanyInTimeRange(Guid companyId, DateTime startTime, DateTime endTime)
+        {
+            var list = await _DbSet
+               .Include(p => p.Vehicle)
+               .Where(p => p.CompanyId == companyId && p.CreateAT >= startTime && p.UpdateAt <= endTime)
+               .ToListAsync();
+            return list;
         }
     }
 }
