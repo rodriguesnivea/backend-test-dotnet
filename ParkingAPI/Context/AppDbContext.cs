b8dd4582e81b89d903fb6453791626aadf8d55ab
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-03-26 21:22:16 -0300

    adicionando Identity para autenticação

--- ParkingAPI/Context/AppDbContext.cs
+++ ParkingAPI/Context/AppDbContext.cs
@@ -1,12 +1,12 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using ParkingAPI.Entities;
 using ParkingAPI.Models;
 
 namespace ParkingAPI.Context
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext() { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
@@ -15,14 +15,14 @@ namespace ParkingAPI.Context
         }
 
         public virtual DbSet<AddressEntity> Address { get; set; }
-
         public virtual DbSet<CompanyEntity> Company { get; set; }
-
         public virtual DbSet<VehicleEntity> Vehicle { get; set; }
         public virtual DbSet<ParkingEntiy> Parking { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<VehicleEntity>(entity =>
             {
                 entity.HasIndex(e => e.Plate)
@@ -35,7 +35,8 @@ namespace ParkingAPI.Context
                     .IsUnique();
             });
 
-            base.OnModelCreating(modelBuilder);
+            // Apenas chame base.OnModelCreating uma vez
+            //base.OnModelCreating(modelBuilder);
         }
     }
 }
--- ParkingAPI/Migrations/20240309011134_add_identity.Designer.cs
+++ ParkingAPI/Migrations/20240309011134_add_identity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ParkingAPI.Context;
 
 namespace ParkingAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240309011134_add_identity")]
+    partial class add_identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
